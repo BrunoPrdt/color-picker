48ef3f4e61fdb9dd7a2518b0d0b2823f6afd425d
BrunoPrdt <bruno.predot@gmail.com>
2020-07-27 14:24:03 +0200

    add Vue Js function / functional app

--- index.js
+++ index.js
@@ -1,6 +1,31 @@
-import Vue from 'vue'
-import './style.css';
 
 let vm = new Vue({
-    el: '#app'
+    el: '#app',
+    data: {
+        green: 0,
+        blue: 0,
+        red: 0,
+        squares: [],
+        squareBackgroundColor: '',
+    },
+    methods: {
+        resetColor() {
+            this.green = this.blue = this.red = 0;
+        },
+        saveQuare() {
+            this.squares.push(`rgb(${this.blue}, ${this.green}, ${this.red})`);
+            console.log('squares', this.squares);
+            this.resetColor();
+        },
+        reset() {
+            this.resetColor();
+            this.squares = [];
+            this.squareBackgroundColor = '';
+        },
+    },
+    computed: {
+        nbrOfSquares() {
+            return this.squares.length;
+        },
+    },
 });
